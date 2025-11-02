32ebf70a7cf7f3e4d755c5ae0069c9d92bbe6092
Omkar Bhongale <omkarbhongle121@gmail.com>
2025-11-02 19:43:13 +0530

    configured PostgreSQL for Render

--- civicissue/settings.py
+++ civicissue/settings.py
@@ -65,26 +65,17 @@ WSGI_APPLICATION = 'civicissue.wsgi.application'
 # ---------------- DATABASE CONFIGURATION ---------------- #
 # Use PostgreSQL on Render, SQLite locally
 
-USE_POSTGRES = os.getenv('USE_POSTGRES', 'False').lower() == 'true'
+import dj_database_url
 
-if USE_POSTGRES:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT', 5432),
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 
 
 # ---------------- PASSWORD VALIDATION ---------------- #
